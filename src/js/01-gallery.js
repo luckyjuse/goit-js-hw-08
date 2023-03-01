dbdad4699c6a4b1d933c68d177789d1fd0624793
Roman Usenko <luckyjuse@gmail.com>
2023-03-01 23:55:09 +0200

    8.1 added

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,5 +2,33 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+        </a>
+    `;
+    })
+    .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  showCounter: false,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
 
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
+
+//  player.on('play', function () {
+//    console.log('played the video!');
+//  });
+
+const player = new Player('vimeo-player', {});
+
+player.on('play', function () {
+  console.log('played the video!');
+});
