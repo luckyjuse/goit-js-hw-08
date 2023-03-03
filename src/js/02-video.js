edb91830b8ddad93242a3f7a795504fe6210e7f8
Roman Usenko <luckyjuse@gmail.com>
2023-03-03 22:39:57 +0200

    8.2 added without throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,8 +7,30 @@ import Player from '@vimeo/player';
 //    console.log('played the video!');
 //  });
 
-const player = new Player('vimeo-player', {});
+const player = new Player('vimeo-player');
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+const currentTimeUpdate = function (data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+};
+
+player.on('timeupdate', currentTimeUpdate);
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
